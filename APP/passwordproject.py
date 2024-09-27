8853b04a1564309267325cc5cc6aaa678149b35b
Daniel Skovgaard <danielskovgaardf95@gmail.com>
2024-09-27 19:46:41 +0200

    updated client app with different navigation code

--- APP/passwordproject.py
+++ APP/passwordproject.py
@@ -46,7 +46,10 @@ def two_factor_qrcode(username=None):
 
 def remove_tfa():
     global logged_in_username
+    console.print("[underline] Skriv \"b\" for at fortryde og vende tilbage til menuen [/underline]\n")
     tfa_code = input("Indtast din 2FA Kode: ")
+    if tfa_code == "b":
+        return
     username = logged_in_username
     headers = {"Content-Type": "application/json"}
     jsondata = {"username": username, "tfa_code": tfa_code}
@@ -169,7 +172,7 @@ def create_user():
 def login(username, password):
     global logged_in_username
     login_info = {"username": username, "password": password}
-    r = s.post("https://api.dfsprojekt.dk/user/login",json=login_info, headers={"Content-Type": "application/json"})
+    r = s.post("https://api.dfsprojekt.dk/user/login", json=login_info, headers={"Content-Type": "application/json"})
 
     if r.status_code == 200 and r.json().get("success"):
         print("Du er nu logget på!")
@@ -297,6 +300,9 @@ def retrieve_password():
 
     service_choice = input("\nHvilken service ønsker du at se password for?: ")
 
+    if service_choice == "b":
+        return "break"
+
     if service_choice not in servicelist["services"]:
         console.print(f"\n\n[underline]{service_choice}[/underline] [bold bright_red]er ikke en gemt service. Vælg venligst en gyldig service.[/bold bright_red]\n\n")
         return False
@@ -364,6 +370,9 @@ def remove_pass():
             print(table)
 
     service_choice = input("\nHvilken account/password ønsker du at fjerne?: ")
+
+    if service_choice == "b":
+        return "break"
 
     if service_choice not in servicelist["services"]:
         print("\n")
@@ -433,47 +442,191 @@ def clear_screen():
     os.system("cls")
 
 
+def start_screen_handler():
+    console.print("[bold yellow]Velkommen til Daniel's Password Manager![/bold yellow]")
+    console.print("[italic]\nLog ind eller opret en konto for at bruge Password Manageren[/italic]")
+    console.print("\n[underline green]1. Log ind[/underline green]")
+    console.print("[underline orange3]2. Opret account[/underline orange3]")
+    console.print("[underline red]3. Exit applikationen[/underline red]")
+    choice_start = input("\n\nHvad ønsker du at gøre?: ")
+    return choice_start
+
+
+def user_login_handler():
+    clear_screen()
+    while True:
+        console.print('[bold]Indtast venligst dine login oplysninger til Daniel\'s Password Manager (Skriv "b" for at vende tilbage til menuen)[/bold]\n')
+        username = input("Username: ")
+        if username == "b":
+            clear_screen()
+            break
+        password = pwinput.pwinput("Password: ")
+        if password == "b":
+            clear_screen()
+            break
+        logged_in = login(username, password)
+        clear_screen()
+        if logged_in:
+            console.print(f"[bold bright_green]Du er nu logget ind som bruger:[/bold bright_green] [bold underline bright_green]{username}[/bold underline bright_green]")
+            return username
+        else:
+            console.print("[bold bright_red]Login mislykkedes![/bold bright_red]")
+
+
+def create_account_handler():
+    clear_screen()
+    console.print("[bold cyan]Opret en konto for at bruge Daniels Password Manager.[/bold cyan]")
+    console.print("[italic]Skriv \"b\" i enten username eller password feltet for at afbryde oprettelsen[/italic]")
+    print("\n")
+    account_created = create_user()
+    if not account_created:
+        console.print("Der opstod et problem, og din account blev ikke oprettet!")
+
+
+def logged_in_menu_handler():
+    list_services()
+    print("\n")
+    console.print("[underline yellow]Du har nu følgende valgmuligheder:[/underline yellow]")
+    console.print("\n")
+    console.print("[bright_green]1. Tilføj et nyt password[/bright_green]")
+    console.print("[bright_green]2. Hent et tidligere gemt password[/bright_green]")
+    console.print("[bright_green]3. Slet account/password fra Password Manager[/bright_green]")
+    print("\n")
+    console.print("[cyan]4. Account indstillinger[/cyan]")
+    console.print("[orange3]5. Log ud[/orange3]")
+    console.print("[bright_red]6. Exit applikationen[/bright_red]")
+    console.print("\n")
+    choice = input("Hvad ønsker du at gøre?: ")
+    return choice
+
+
+def account_settings_menu():
+    console.print(f"[underline]Account indstillinger for {username}:[/underline]")
+    console.print("[italic]Skriv \"b\" i enten username eller password feltet for at afbryde oprettelsen[/italic]")
+    print("\n")
+    console.print("1. Tilføj 2FA (2-Factor-Authentication) til din account")
+    console.print("2. Fjern 2FA fra din account")
+    console.print("3. Slet account")
+    print("\n")
+    choice = input("Hvad ønsker du at gøre?: ")
+    return choice
+
+
+def add_service_handler():
+    clear_screen()
+    while True:
+        if add_service() == "break":
+            clear_screen()
+            break
+        print("\n")
+        user_pick = input("Vil du tilføje flere passwords? (j/n): ")
+        if user_pick == "n":
+            clear_screen()
+            break
+        clear_screen()
+
+
+def retrieve_password_handler():
+    clear_screen()
+    while True:
+        retrieve = retrieve_password()
+        if retrieve is True:
+            print("\n")
+            user_pick = input("Ønsker du at finde et nyt password? (j/n): ")
+            if user_pick == "n":
+                clear_screen()
+                break
+
+        elif retrieve == "break":
+            clear_screen()
+            break
+
+        else:
+            console.print("\n[bold bright_red]Du har ingen gemte passwords.[/bold bright_red]")
+            console.print("[italic]Tryk på en tast for at forsætte.....[/italic]")
+            msvcrt.getch()
+            clear_screen()
+            break
+
+
+def remove_password_handler():
+    clear_screen()
+    while True:
+        remove = remove_pass()
+        if remove is True:
+            print("\n")
+            user_pick = input("Ønsker du at fjerne flere passwords? (j/n): ")
+            if user_pick == "n":
+                clear_screen()
+                break
+            clear_screen()
+
+        elif remove == "break":
+            clear_screen()
+            break
+
+        else:
+            console.print("\n[bold bright_red]Du har ingen gemte passwords.[/bold bright_red]")
+            console.print("[italic]Tryk på en tast for at forsætte.....[/italic]")
+            msvcrt.getch()
+            clear_screen()
+            break
+
+
+def account_settings_handler():
+    clear_screen()
+    while True:
+        account_settings_choice = account_settings_menu()
+        if account_settings_choice == "1":
+            clear_screen()
+            two_factor_qrcode()
+            break
+
+        elif account_settings_choice == "2":
+            clear_screen()
+            remove_tfa()
+            break
+
+        elif account_settings_choice == "3":
+            clear_screen()
+            delete_user()
+            break
+
+        elif account_settings_choice == "b":
+            clear_screen()
+            break
+
+        else:
+            clear_screen()
+            console.print("Ikke et gyldigt valg, prøv igen!.")
+
+
+def logout_handler():
+    logout()
+    clear_screen()
+
+
+options_start_screen = {
+    "1": user_login_handler,
+    "2": create_account_handler,
+    "3": "exit"
+}
+
+
 if __name__ == "__main__":
     logged_in = False
     while True:
         if not s.cookies:
-            console.print("[bold yellow]Velkommen til Daniel's Password Manager![/bold yellow]")
-            console.print("[italic]\nLog ind eller opret en konto for at bruge Password Manageren[/italic]")
-            console.print("\n[underline green]1. Log ind[/underline green]")
-            console.print("[underline orange3]2. Opret account[/underline orange3]")
-            console.print("[underline red]3. Exit applikationen[/underline red]")
-            choice_start = input("\n\nHvad ønsker du at gøre?: ")
-            if choice_start == "1":
-                clear_screen()
-                while True:
-                    console.print('[bold]Indtast venligst dine login oplysninger til Daniel\'s Password Manager (Skriv "b" for at vende tilbage til menuen)[/bold]\n')
-                    username = input("Username: ")
-                    if username == "b":
-                        clear_screen()
-                        break
-                    password = pwinput.pwinput("Password: ")
-                    if password == "b":
-                        clear_screen()
-                        break
-                    logged_in = login(username, password)
-                    clear_screen()
-                    if logged_in:
-                        console.print(f"[bold bright_green]Du er nu logget ind som bruger:[/bold bright_green] [bold underline bright_green]{username}[/bold underline bright_green]")
-                        break
-                    else:
-                        console.print("[bold bright_red]Login mislykkedes![/bold bright_red]")
+            choice = start_screen_handler()
+            action = options_start_screen.get(choice)
 
-            elif choice_start == "2":
-                clear_screen()
-                console.print("[bold cyan]Opret en konto for at bruge Daniels Password Manager.[/bold cyan]")
-                console.print("[italic]Skriv \"b\" i enten username eller password feltet for at afbryde oprettelsen[/italic]")
-                print("\n")
-                account_created = create_user()
-                if not account_created:
-                    console.print("Der opstod et problem, og din account blev ikke oprettet!")
-
-            elif choice_start == "3":
+            if action == "exit":
                 break
+            elif action:
+                if action == user_login_handler:
+                    username = action()
+                else:
+                    action()
 
             else:
                 clear_screen()
@@ -483,107 +636,25 @@ if __name__ == "__main__":
             if not s.cookies:
                 print("Du skal være logget ind for at kunne bruge password manageren!")
                 break
-            list_services()
-            print("\n")
-            console.print("[underline yellow]Du har nu følgende valgmuligheder:[/underline yellow]")
-            console.print("\n")
-            console.print("[bright_green]1. Tilføj et nyt password[/bright_green]")
-            console.print("[bright_green]2. Hent et tidligere gemt password[/bright_green]")
-            console.print("[bright_green]3. Slet account/password fra Password Manager[/bright_green]")
-            print("\n")
-            console.print("[cyan]4. Account indstillinger[/cyan]")
-            console.print("[orange3]5. Log ud[/orange3]")
-            console.print("[bright_red]6. Exit applikationen[/bright_red]")
-            console.print("\n")
-            choice = input("Hvad ønsker du at gøre?: ")
-            if choice == "1":
-                clear_screen()
-                while True:
-                    if add_service() == "break":
-                        clear_screen()
-                        break
-                    print("\n")
-                    user_pick = input("Vil du tilføje flere passwords? (j/n): ")
-                    if user_pick == "n":
-                        clear_screen()
-                        break
+
+            logged_in_choice = logged_in_menu_handler()
+            logged_in_actions = {
+                "1": add_service_handler,
+                "2": retrieve_password_handler,
+                "3": remove_password_handler,
+                "4": account_settings_handler,
+                "5": logout_handler,
+                "6": "exit_application"
+            }
+
+            action = logged_in_actions.get(logged_in_choice)
+            if action:
+                if action == "exit_application":
                     clear_screen()
-
-            elif choice == "2":
-                clear_screen()
-                while True:
-                    retrieve = retrieve_password()
-                    if retrieve is True:
-                        print("\n")
-                        user_pick = input("Ønsker du at finde et nyt password? (j/n): ")
-                        if user_pick == "n":
-                            clear_screen()
-                            break
-                    else:
-                        console.print("\n[bold bright_red]Du har ingen gemte passwords.[/bold bright_red]")
-                        console.print("[italic]Tryk på en tast for at forsætte.....[/italic]")
-                        msvcrt.getch()
-                        clear_screen()
-                        break
-
-            elif choice == "3":
-                clear_screen()
-                while True:
-                    remove = remove_pass()
-                    if remove is True:
-                        print("\n")
-                        user_pick = input("Ønsker du at fjerne flere passwords? (j/n): ")
-                        if user_pick == "n":
-                            clear_screen()
-                            break
-                        clear_screen()
-                    else:
-                        console.print("\n[bold bright_red]Du har ingen gemte passwords.[/bold bright_red]")
-                        console.print("[italic]Tryk på en tast for at forsætte.....[/italic]")
-                        msvcrt.getch()
-                        clear_screen()
-                        break
-            elif choice == "4":
-                clear_screen()
-                while True:
-                    console.print(f"[underline]Account indstillinger for {username}:[/underline]")
-                    console.print("[italic]Skriv \"b\" i enten username eller password feltet for at afbryde oprettelsen[/italic]")
-                    print("\n")
-                    console.print("1. Tilføj 2FA (2-Factor-Authentication) til din account")
-                    console.print("2. Fjern 2FA fra din account")
-                    console.print("3. Slet account")
-                    print("\n")
-                    settings_choice = input("Hvad ønsker du at gøre?: ")
-                    if settings_choice == "1":
-                        clear_screen()
-                        two_factor_qrcode()
-                        break
-
-                    elif settings_choice == "2":
-                        clear_screen()
-                        remove_tfa()
-                        break
-
-                    elif settings_choice == "3":
-                        clear_screen()
-                        delete_user()
-                        break
-
-                    elif settings_choice == "b":
-                        clear_screen()
-                        break
-
-                    else:
-                        console.print("Ikke et gyldigt valg, prøv igen!.")
-
-            elif choice == "5":
-                logout()
-                clear_screen()
-
-            elif choice == "6":
-                logout()
-                break
-
+                    logout()
+                    break
+                else:
+                    action()
             else:
                 clear_screen()
-                console.print("[bold bright_red]Dette er ikke en valgmulighed! Vælg en af de opstillede muligheder[/bold bright_red]")
+                console.print("[bold bright_red]Ikke en valgmulighed, prøv igen.[/bold bright_red]")
